75fde6a4e5c23ec71a0d553bdd2ec867ee7ec77e
ayush.zerror@gmail.com <ayush.zerror@gmail.com>
2025-01-07 14:49:35 +0530

    video compressed

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const HeroSection = () => {
   return (
     <div className='w-full h-[100svh] relative overflow-hidden'>
-        <video autoPlay muted loop playsInline className='sm:hidden block w-full h-full object-cover object-top' src="/heroVid.mp4"></video>
-        <video autoPlay muted loop playsInline className='sm:block hidden w-full h-full object-cover object-top' src="/heroVidPhn.mp4"></video>
+        <video autoPlay muted loop playsInline className='sm:hidden md:hidden block w-full h-full object-cover object-top' src="/heroVid.mp4"></video>
+        <video autoPlay muted loop playsInline className='sm:block md:block hidden w-full h-full object-cover object-top' src="/heroVidPhn.mp4"></video>
         <div className='w-full flex items-center justify-center flex-col h-screen absolute top-0 left-0'>
             <img src="/text_logo.avif" className='sm:h-[4vw] md:h-[3vw] lg:h-[2.6vw] h-[2vw] sm:top-[6vw] md:top-[4vw] lg:top-[4vw] md:left-[6vw] lg:left-[6vw] sm:left-1/2 sm:-translate-x-1/2 top-[2vw] left-[3.5vw] absolute' alt="" />
             <h1 className='soon sm:text-[8vw] md:text-[7.2vw] lg:text-[6vw] xl:text-[4.8vw]  text-[3.5vw] uppercase' style={{fontWeight:"900"}}>Coming Soon</h1>

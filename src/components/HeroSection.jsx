f58cdace8b25ab9d999f4100efce9250f1a3ce3f
ayush.zerror@gmail.com <ayush.zerror@gmail.com>
2025-01-07 15:01:14 +0530

    everything updated

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -8,7 +8,7 @@ const HeroSection = () => {
         <div className='w-full flex items-center justify-center flex-col h-screen absolute top-0 left-0'>
             <img src="/text_logo.avif" className='sm:h-[4vw] md:h-[3vw] lg:h-[2.6vw] h-[2vw] sm:top-[6vw] md:top-[4vw] lg:top-[4vw] md:left-[6vw] lg:left-[6vw] sm:left-1/2 sm:-translate-x-1/2 top-[2vw] left-[3.5vw] absolute' alt="" />
             <h1 className='soon sm:text-[8vw] md:text-[7.2vw] lg:text-[6vw] xl:text-[4.8vw]  text-[3.5vw] uppercase' style={{fontWeight:"900"}}>Coming Soon</h1>
-            <p className='sm:text-[3vw] md:text-[2.8vw] lg:text-[2vw] xl:text-[1.8vw] text-[1.2vw] sm:font-semibold font-regular  font-[font1] sm:mb-[8vh]'>A Brand New Website Is Coming Soon!</p>
+            <p className='sm:text-[3vw] md:text-[2.8vw] lg:text-[2vw] xl:text-[1.8vw] text-[1.2vw] sm:font-semibold font-regular  font-[font1] sm:mb-[12vh]'>A Brand New Website Is Coming Soon!</p>
         </div>
     </div>
   )
